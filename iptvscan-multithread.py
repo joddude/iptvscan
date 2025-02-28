394b235fb924302a035c19e56689dcc45dafd3c1
joddude <16442923+joddude@users.noreply.github.com>
2025-02-28 21:06:34 +0200

    add iptvscan-multithread.py

--- iptvscan-multithread.py
+++ iptvscan-multithread.py
@@ -33,6 +33,8 @@ ports    = [1234, 1235, 1238, 1239, 1250, 1251, 1252, 1281, 1282, 1283, 1284,
 timeout=1               # seconds
 random_search = False   # False or True
 
+num_threads = 10
+
 #------------------------------------------------------------------------------
 
 import socket
@@ -40,6 +42,8 @@ import struct
 import os, sys
 import time, datetime
 import random
+import threading
+import queue
 
 #------------------------------------------------------------------------------
 
@@ -56,22 +60,52 @@ def main():
     print('Playlist name:', playlist_name, '\n')
     with open(playlist_name, 'w') as f:
         f.write('#EXTM3U\n\n')
-    found_channels = 0
-    update_progress(0, 'Scan '+ip_start+':'+str(ports[0]))
-    for counter, scan_item in enumerate(scan_list, start=1):
-        if iptv_test(scan_item['ip'], scan_item['port'], timeout):
-            write_to_playlist(scan_item['ip'], str(scan_item['port']))
-            found_channels +=1
-        update_progress(counter/len(scan_list), 'Scan '+scan_item['ip']+':'+str(scan_item['port']), '(Found '+str(found_channels)+' channels)    ')
-    print(f'\nFound {found_channels} channels.')
+    found_channels = [0]
+    scan_queue = queue.Queue()
+    lock = threading.Lock()
+    stop_event = threading.Event()
+    for scan_item in scan_list:
+        scan_queue.put(scan_item)
+    threads = []
+    for i in range(num_threads):
+        t = threading.Thread(target=scan_thread, args=(scan_queue, found_channels, lock, stop_event))
+        t.start()
+        threads.append(t)
+    try:
+        while not scan_queue.empty():
+            time.sleep(1)
+            progress = (len(scan_list) - scan_queue.qsize()) / len(scan_list)
+            update_progress(progress, 'Scanning', f'(Found {found_channels[0]} channels)')
+    except KeyboardInterrupt:
+        print('\n\nYou pressed Ctrl+C. Stop...')
+        stop_event.set()
+        scan_queue.queue.clear()
+    for t in threads:
+        t.join()
+    print(f'\nFound {found_channels[0]} channels.')
 
 #------------------------------------------------------------------------------
 
-def write_to_playlist(ip, port):
-    with open(playlist_name, 'a') as f:
-        f.write(f'#EXTINF:-1,IPTV Channel {ip}:{port}\n')
-        f.write(f'udp://@{ip}:{port}\n')
-        f.write('\n')
+def scan_thread(q, found_channels, lock, stop_event):
+    while not stop_event.is_set():
+        try:
+            scan_item = q.get(timeout=1)
+            if iptv_test(scan_item['ip'], scan_item['port'], timeout=1):
+                write_to_playlist(scan_item['ip'], scan_item['port'], lock)
+                with lock:
+                    found_channels[0] += 1
+            q.task_done()
+        except queue.Empty:
+            break
+
+#------------------------------------------------------------------------------
+
+def write_to_playlist(ip, port, lock):
+    with lock:
+        with open(playlist_name, 'a') as f:
+            f.write(f'#EXTINF:-1,IPTV Channel {ip}:{port}\n')
+            f.write(f'udp://@{ip}:{port}\n')
+            f.write('\n')
 
 #------------------------------------------------------------------------------
 
@@ -137,9 +171,6 @@ if __name__ == '__main__':
     try:
         print('IPTV scan started. Press Ctrl+C to stop.\n')
         main()
-    except KeyboardInterrupt:
-        print('\n\nYou pressed Ctrl+C. Stop')
-        sys.exit()
     except:
         import sys
         print(sys.exc_info()[0])
